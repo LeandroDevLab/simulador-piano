2029cf022dfd04fd882a80e12a0e6e0739c94733
LeandroDevLab <prof.oliota@gmail.com>
2025-04-17 11:43:46 -0300

    fix: mapeando as teclas permitidas e evitando erro de console

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let mapedKeys = [];
+
 let audio = new Audio("src/tunes/a.wav");
 
 const playTune = (key) => {
@@ -15,12 +17,20 @@ const playTune = (key) => {
 };
 
 pianoKeys.forEach((key)=>{
-    console.log(key.dataset.key);
-    key.addEventListener("click", ()=> playTune(key.dataset.key))
+    /* console.log(key.dataset.key); */
+    key.addEventListener("click", ()=> playTune(key.dataset.key));
+    
+    /* como o forEach passou por todas as teclas de data-key, ele adicionou com o push todas elas dentro do vetor mapedKeys */
+    mapedKeys.push(key.dataset.key); 
 });
 
 document.addEventListener("keydown", (e) => {
     /* console.log(e); */ /* KeyboardEvent captura várias informações das teclas pressionadas (sem a .key)*/
     // console.log(e.key);
-    playTune(e.key);
+
+    /* Só irá chamar o playTune IF e.key estiver contido, includes, dentro da variável(vetor) mapedKeys */
+    if(mapedKeys.includes(e.key)) {
+        playTune(e.key);
+    }
+    
 });
